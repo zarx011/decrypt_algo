b2753b735ccd7e0794ac5c96f16a6ae79e283430
zarx011 <129439378+zarx011@users.noreply.github.com>
2024-10-14 16:03:05 -0400

    TODO comments

--- backtrack.py
+++ backtrack.py
@@ -1,3 +1,7 @@
+# seems to stop checking once key_len = 6 even though max set at 10
+# doesn't seem to work for keys other than "key"
+# something is weird with it in general but not sure exactly what yet
+
 import string
 
 # func to decrypt a char w/ key
